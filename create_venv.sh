179256fbe1139b812613fa4d6b3e673598b0e80e
Johann A. Briffa <johann.briffa@um.edu.mt>
2020-10-23 11:18:43 +0200

    Ensure venv is created in this folder

--- create_venv.sh
+++ create_venv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+FOLDER=$(dirname ${BASH_SOURCE[0]})
+cd ${FOLDER}
 python3 -m venv --system-site-packages --prompt temper venv
-source venv/bin/activate
-python setup.py install
+source venv/bin/activate && python setup.py install
